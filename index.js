3a163e567acc685b5d138cb5f1f175a4150b52f2
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-07 11:40:19 -0500

    commit tree 760154c17a1a02679a151e818f1acdd741cd6018

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(username, useremail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${username}`,
+            email: `${useremail}`,
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (obj) {
+        document.body.innerHTML = `${obj.id}`
+    })
+    .catch(function (message) {
+        document.body.innerHTML = `${message}`
+    })
+}
+
+// .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
